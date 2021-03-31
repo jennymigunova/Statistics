7c77133c4a1838f218a7a036fa0d3195a946c798
Evgeniya Migunova <8880305@gmail.com>
2021-03-31 19:14:50 -0400

    Add Test

---
+++ src/test/java/ru/netology/stats/StatisticsTest.java
@@ -0,0 +1,30 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsTest {
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void middleSum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void maxSum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minSum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void belowMidSum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void overMidSum() {
+    }
+}
